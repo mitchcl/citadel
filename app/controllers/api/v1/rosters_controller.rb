93501e74ee6d1b58b052cc575d7732cac46a525a
Shigbeard <4262327+Shigbeard@users.noreply.github.com>
2024-11-04 10:16:26 +1100

    feat: API endpoint for determining if a roster is active.

--- app/controllers/api/v1/rosters_controller.rb
+++ app/controllers/api/v1/rosters_controller.rb
@@ -6,6 +6,18 @@ module API
 
         render json: @roster, serializer: V1::Leagues::RosterSerializer
       end
+
+      def active
+        @roster = League::Roster.includes(division: :league).find(params[:id])
+
+        league_status = @roster.division.league&.status
+        is_active = league_status.present? &&  league_status == 'running'
+
+        render json: {
+          id: @roster.id,
+          active: is_active
+        }
+      end
     end
   end
 end
